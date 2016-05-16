e4c6203ed4f1ad9d29d194926971d16413ae4b57
Niels de Vos <ndevos@redhat.com>
2016-05-16 15:19:17 +0200

    release_notes: generate Markdown formatted notes (#4)
    This makes it a little easier to include the notes in a .md file that
    can be placed in the <glusterfs>/doc/release-notes/ directory.
    
    Added a bit more comments about the working of the script as well.
    
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- release_notes.sh
+++ release_notes.sh
@@ -1,22 +1,39 @@
-#!/bin/sh
-
+#!/bin/bash
+#
+# Generate release notes in Markdown format so that they can easily be included
+# in the <glusterfs-repo>/docs/release-notes/ directory.
+#
+# This script expects the following parameters:
+#
+# 1: base version (git commit, tag, ..)
+# 2: target version these release notes are for (the git commit, tag ..)
+# 3: path to the git repository
+#
+# While this script runs, the output is printed to stdout. Afterwards the
+# results can also be found in /tmp/release_notes.
+#
 
 function generate_release_notes ()
 {
-    orig_version=$1
-    latest_version=$2
-    repo=$3;
+    local orig_version=$1
+    local latest_version=$2
+    local repo=$3
 
-    cd $3;
+    cd ${repo}
 
-for i in $(git log $1..$2 | grep BUG | grep -v ">" | cut -f 2 -d ":") ;do
-    echo -n "$i - " >> /tmp/release_notes && echo `bugzilla query -b $i | sed -e 's/.*\ -\ //'` >> /tmp/release_notes;
-done
-
-# the below style of using awk can also be used as it acts as the logical cut with delimiter being ' - '
-#for i in $(git log $1..$2 | grep BUG | grep -v ">" | cut -f 2 -d ":") ;do
-#    echo -n "$i - " >> /tmp/release_notes && echo `bugzilla query -b $i | awk -F' - ' '{print $3'}` >> /tmp/release_notes;
-#done
+    # step 1: gather all BUG numbers from the commit messages
+    #         use format=email so that BUG: is at the start of the line
+    # step 2: split the BUG: lines at the : position, only return the 2nd part
+    # step 3: filter non-numeric lines, and strip off any spaces with awk
+    # step 4: sort numeric, and only get occurences once (-u for unique)
+    # step 5: use xargs to pass some of the bugs to the bugzilla command
+    # step 6: show progress on the current terminal, and write to a file
+    git log --format=email $1..$2 | grep -w -i ^BUG \
+        | cut -d: -f2 \
+        | awk '/^[[:space:]]*[0-9]+[[:space:]]*$/{print $1}' \
+        | sort -n -u \
+        | xargs -r -n1 bugzilla query --outputformat='- [#%{id}](https://bugzilla.redhat.com/%{id}): %{summary}' -b \
+        | tee /tmp/release_notes
 }
 
 function main ()
